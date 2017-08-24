992fbc3ce772b0fb2b09f86c7907deecb534465a
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-08-23 19:25:25 -0500

    Add cpu usage endpoint

--- api/app.py
+++ api/app.py
@@ -1,18 +1,47 @@
 import os
 
-from apistar import App, Route
+from apistar.frameworks.wsgi import WSGIApp as App
+from apistar.http import Response, Request
+from apistar import Route
+from psutil import cpu_percent
+
+
+class CORSApp(App):
+    """
+    Wraps an app, so that it injects CORS headers.
+    """
+
+    def finalize_response(self, response: Response) -> Response:
+        """
+        Inject cors headers and finalize.
+        """
+        response = super().finalize_response(response)
+        content, status, headers, content_type = response
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Authorization'
+        headers['Access-Control-Allow-Credentials'] = 'true'
+        headers['Access-Control-Allow-Methods'] = 'GET'
+        return Response(content, status, headers, content_type)
 
 
 def greet():
+    """
+    Greet our user.
+    """
     return {
         'message': 'Hi there fella',
         'hostname': os.environ.get('HOSTNAME'),
     }
 
 
+def cpu():
+    return cpu_percent(interval=1, percpu=True)
+
+
 ROUTES = [
     Route('/', 'GET', greet),
+    Route('/cpu', 'GET', cpu),
 ]
 
 
-app = App(routes=ROUTES)
+app = CORSApp(routes=ROUTES)
